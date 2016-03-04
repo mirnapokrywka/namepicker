c8af53b6dc8a8cc07a07e0e02a06d10e6f7dc06c
Mirna <mirnapokrywka@gmail.com>
2016-03-04 10:21:34 -0800

    copied the formula again

---
+++ app.js
@@ -0,0 +1,13 @@
+var codeTalkNames = [];
+
+function resetNames(){
+	codeTalkNames = ['Bernice', 'Elliette', 'Mirna', 'Erinn', 'Aimee', 'Yadira', 'Janene', 'Denise'];
+
+}
+
+function namePicker(){
+	var namePick = codeTalkNames.splice(Math.floor(Math.random() * codeTalkNames.length), 1);
+}
+function namePicker(){
+	var namePick = codeTalkNames.splice(Math.floor(Math.random() * codeTalkNames.length), 1);
+}
